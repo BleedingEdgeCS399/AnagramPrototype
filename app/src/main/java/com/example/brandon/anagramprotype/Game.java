0aadde363005d5f75e675e5755fb2b0d0051e51b
Christian Buskirk <i2amroy@gmail.com>
2015-10-19 15:33:32 -0700

    Get compilation up and working

--- app/src/main/java/com/example/brandon/anagramprotype/Game.java
+++ app/src/main/java/com/example/brandon/anagramprotype/Game.java
@@ -3,27 +3,22 @@ package com.example.brandon.anagramprotype;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.logging.Handler;
+import android.os.Handler;
 
 public class Game extends AppCompatActivity {
 
-    String[] EasyArray={"bugs"};
-    String[] MediumArray={"Trash"};
-    String[] HardArray={"Fanastic"};
+    String[] EasyArray = {"bugs"};
+    String[] MediumArray = {"trash"};
+    String[] HardArray = {"fantastic"};
     int mode;
-    int score=0;
-    int puzzles=0;
+    int score = 0;
+    int puzzles = 0;
     String Answer;
 
     TextView time;
@@ -37,24 +32,17 @@ public class Game extends AppCompatActivity {
     boolean stopTimer = false;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        startTime= SystemClock.uptimeMillis();
-        customHandler.postDelayed(updateTimerThread,0);
-
-
         Button title_return = (Button) findViewById(R.id.MainActReturn);
         final Button score_btn = (Button) findViewById(R.id.Score_Btn);
 
-        final Intent intentScore= new Intent(Game.this,Score_Menu.class);
-        final EditText answer= (EditText) findViewByID(R.id.answer);
+        final Intent intentScore = new Intent(Game.this, Score_Menu.class);
+        final EditText answer = (EditText) findViewById(R.id.AnswerString);
         title_return.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v) {
 
                 Answer = answer.getText().toString();
@@ -100,35 +88,38 @@ public class Game extends AppCompatActivity {
                         }
 
                     });
-
-
-                    Runnable updateTimerThread = new Runnable() {
-
-                        public void run() {
-                            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
-
-                            updatedTime = timeSwapBuff + timeInMilliseconds;
-
-                            int secs = (int) (updatedTime / 1000);
-                            int mins = secs / 60;
-                            secs = secs % 60;
-                            int milliseconds = (int) (updatedTime % 1000);
-                            String localtime = "" + mins + ":" + String.format("%02d", secs)
-                                    + ":" + String.format("%03d", milliseconds);
-                            time.setText(localtime);
-                            if (mins == 1) {
-                                stopTimer = true;
-                                Toast.makeText(Game.this, "You guys are awesome",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                            if (!stopTimer)
-                                customHandler.postDelayed(this, 0);
-                        }
-                    };
-
-
                 }
+            }
+        });
 
 
+        Runnable updateTimerThread = new Runnable() {
 
-}
+            public void run() {
+                timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+                updatedTime = timeSwapBuff + timeInMilliseconds;
+
+                int secs = (int) (updatedTime / 1000);
+                int mins = secs / 60;
+                secs = secs % 60;
+                int milliseconds = (int) (updatedTime % 1000);
+                String localtime = "" + mins + ":" + String.format("%02d", secs)
+                        + ":" + String.format("%03d", milliseconds);
+                time.setText(localtime);
+                if (mins == 1) {
+                    stopTimer = true;
+                    Toast.makeText(Game.this, "You guys are awesome",
+                            Toast.LENGTH_SHORT).show();
+                }
+                if (!stopTimer)
+                    customHandler.postDelayed(this, 0);
+
+
+            }
+        };
+
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread, 0);
+    }
+};
